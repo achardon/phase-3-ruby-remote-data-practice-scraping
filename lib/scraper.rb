206d38c3b924424c5751c991af8bf84be3c65aed
Alessandra Chardon <achardon@ucla.edu>
2021-12-27 10:21:14 -0800

    done

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,18 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+
+puts "headline: ", doc.css(".headline-26OIBN").text.strip
+
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+
+puts "Courses: "
+
+courses.each do |course|
+  puts course.css(".title-oE5vT4").text
+  
+end
